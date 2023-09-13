d1778e19b59f3d4156dca5558c6a2734764be034
Isabelle Blixt <108222104+Bella0g@users.noreply.github.com>
2023-09-13 12:57:59 +0200

    added folders and basic header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import MainPage from './pages/MainPage';
+import postDetails from './pages/PostDetails';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <MainPage/>
     </div>
   );
 }
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import "./Header.css"
 
-export default function header() {
+export default function Header() {
   return (
-    <div>
-      
-    </div>
+    <header>
+      <img className='logo' src="./Reddit-logo.png" alt="reddit-logo" />
+      <div>
+      <h2>All Subreddits</h2>
+      <nav>
+        <ul>
+          <li><a href="/">Hot</a></li>
+          <li><a href="/about">New</a></li>
+          <li><a href="/contact">Controversial</a></li>
+        </ul>
+      </nav>
+      </div>
+    </header>
   )
 }

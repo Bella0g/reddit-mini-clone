a470cc1cab5e11a4e8f808236b8852a0a5cf354f
Isabelle Blixt <108222104+Bella0g@users.noreply.github.com>
2023-09-13 13:53:50 +0200

    finished basic styling of header

---
+++ src/components/header/HeaderTwo.jsx
@@ -0,0 +1,12 @@
+export default function HeaderTwo() {
+  return (
+    <header>
+      <img className='logo' src="./Reddit-logo.png" alt="reddit-logo" />
+      <nav>
+        <ul>
+          <li><a href="/contact">Back to FrontPage</a></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,13 +5,16 @@ export default function Header() {
   return (
     <header>
       <img className='logo' src="./Reddit-logo.png" alt="reddit-logo" />
-      <div>
+      <div className='headerContainer'>
       <h2>All Subreddits</h2>
       <nav>
         <ul>
-          <li><a href="/">Hot</a></li>
-          <li><a href="/about">New</a></li>
-          <li><a href="/contact">Controversial</a></li>
+          <li><a href="/">HOT</a></li>
+          <li><a href="/">NEW</a></li>
+          <li><a href="/">CONTROVERSIAL</a></li>
+          <li><a href="/">RISING</a></li>
+          <li><a href="/">MORE</a></li>
+          
         </ul>
       </nav>
       </div>

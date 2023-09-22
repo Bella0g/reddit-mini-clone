8a52ebe38b45b1e8ad98a325756e47e47044ff55
DaniellaIsaksson <daniellas.isaksson@gmail.com>
2023-09-22 23:41:07 +0200

    Fetched comments and displayed it to the website

--- src/components/comments/coments.jsx
+++ src/components/comments/coments.jsx
@@ -1,2 +1,34 @@
-// Comments component: This component will display all the comments for a post. 
+// Comments component: This component will display all the comments for a post.
 // It will show the content of the comment and the name of the creator (user).
+
+import { useEffect, useState } from "react";
+
+function Coments() {
+  const [comment, setComment] = useState([]);
+  useEffect(() => {
+    getComment();
+  }, []);
+
+  const getComment = async () => {
+    const api = await fetch("https://dummyjson.com/comments");
+    const commentData = await api.json();
+    console.log(commentData);
+    setComment(commentData.comments);
+  };
+  return (
+    <>
+      {comment.map((comment) => {
+        return (
+          <>
+            <ul key={comment.id}>
+              <p>{comment.body}</p>
+              <p>{comment.user.username}</p>
+            </ul>
+          </>
+        );
+      })}
+    </>
+  );
+}
+
+export default Coments;

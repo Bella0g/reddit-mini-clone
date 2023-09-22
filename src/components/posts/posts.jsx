5110279a75bc67136855eadbce82bcdee394d90b
Isabelle Blixt <108222104+Bella0g@users.noreply.github.com>
2023-09-22 15:40:13 +0200

    started adding posts and card componets to render post on mainPage

--- src/components/posts/posts.jsx
+++ src/components/posts/posts.jsx
@@ -1,2 +1,30 @@
 //  This component will display all the posts on the main page. 
-//It will show the title, a part of the content (max 60 characters), tags, and the name of the creator (user).
+// It will show the title, a part of the content (max 60 characters), tags, and the name of the creator (user).
+import React, { useState, useEffect } from "react";
+import Card from "../card/Card";
+import "./Posts.css";
+
+export default function Posts() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        fetch("https://dummyjson.com/posts")
+            .then((response) => response.json())
+            .then((data) => {
+                setPosts(data);
+            })
+            .catch((error) => {
+                console.error("Error fetching posts:", error);
+            });
+    }, []);
+
+    return (
+        <div className="container">
+            {posts.length > 0 ? (
+                posts.map((post) => <Card key={post.id} post={post} />)
+            ) : (
+                <p>Loading posts...</p>
+            )}
+        </div>
+    );
+}
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import Header from '../components/Header/Header';
+import Header from '../components/header/Header';
 import { CreatePostForm } from '../components/create_post/createPost';
+import Posts from "../components/posts/Posts"
 
 export default function MainPage() {
   return (
     <>
     <Header />
     <CreatePostForm/>
+    <Posts/>
     </>
   )
 }

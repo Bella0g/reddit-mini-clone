53a0581382ed1f4b42eaf16c5b079b55efd8f216
Isabelle Blixt <108222104+Bella0g@users.noreply.github.com>
2023-09-27 17:41:55 +0200

    new version of css form header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import DropDown from './DropDown'
-import "./Header.css"
+import DropDown from './DropDown' // Import the DropDown component
+import "./Header.css" // Create a separate CSS file for styling
 
 export default function Header() {
   
   return (
     <nav>
-      <img className='logo' src="./Reddit-logo.png" alt="reddit-logo" />
-      <div className='headerContainer'>
+      <div className="image-container">
+      <img src="./Reddit-logo.png" alt="reddit-logo" />
+      </div>
+      <div className='header-container'>
         <ul className='header-ul'>
           <li><a href="/">HOT</a></li>
           <li><a href="/">NEW</a></li>

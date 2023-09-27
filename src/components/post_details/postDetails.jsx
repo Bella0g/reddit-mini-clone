79b3515e3994e7025c816f616f681fd4842d055f
Isabelle Blixt <108222104+Bella0g@users.noreply.github.com>
2023-09-27 13:09:19 +0200

    removed file

--- src/components/post_details/postDetails.jsx
+++
@@ -1,2 +0,0 @@
-// This component will display the full details of a post when a user clicks on it.
-// It will show the title, the full content, tags, the name of the creator (user), the number of reactions, and the comments.

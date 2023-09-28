3e85a564cef23ead2aba1bc95db8c7bc0cfea5f1
Isabelle Blixt <108222104+Bella0g@users.noreply.github.com>
2023-09-28 09:55:47 +0200

    started adding functionality to header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,23 +1,47 @@
-import React from 'react'
-import DropDown from './DropDown' // Import the DropDown component
-import "./Header.css" // Create a separate CSS file for styling
+import React, { useState } from 'react';
+import PostList from '../posts/posts';
+import "./Header.css"
+
+const topics = ['fiction', 'mystery', 'history', 'crime', 'love'];
 
 export default function Header() {
-  
+  const [selectedPosts, setSelectedPosts] = useState(null);
+
+  const handleTopicClick = (topic) => {
+    fetch(`https://dummyjson.com/posts/search?q=${topic}`)
+      .then(res => res.json())
+      .then(posts => {
+        console.log('Received posts:', posts); // Log the received post data
+        setSelectedPosts(posts);
+      })
+      .catch(error => console.error(`Error fetching posts for topic ${topic}:`, error));
+  }
+
   return (
+    <div>
     <nav>
       <div className="image-container">
-      <img src="./Reddit-logo.png" alt="reddit-logo" />
+        <img src="./Reddit-Logo.png" alt="reddit-logo" />
       </div>
       <div className='header-container'>
         <ul className='header-ul'>
-          <li><a href="/">HOT</a></li>
-          <li><a href="/">NEW</a></li>
-          <li><a href="/">CONTROVERSIAL</a></li>
-          <li><a href="/">ENTERTAINMENT</a></li>
-             <DropDown/>
+            {topics.map(topic => (
+              <li key={topic}>
+                <a onClick={() => handleTopicClick(topic)}>
+                  {topic.toUpperCase()}
+                </a>
+              </li>
+            ))}
         </ul>
       </div>
-    </nav >
+    </nav>
+      {
+        selectedPosts ? (
+          <PostList posts={selectedPosts} />
+        ) : (
+          <PostList />
+        )
+      }
+    </div>
   )
-}
+};
